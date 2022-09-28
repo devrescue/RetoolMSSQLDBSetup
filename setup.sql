91a3e414096f7a5130e9ca50cd0766278059563b
devrescue <ko.devrescue@gmail.com>
2022-09-28 14:00:58 -0400

    removed view and changed primary key

--- setup.sql
+++ setup.sql
@@ -112,141 +112,21 @@ INSERT INTO NationalParkList(park_area_acres,park_area_sq_km,park_lat,park_long,
 USE [RetoolSampleDataDB]
 GO
 
-/****** Object:  View [dbo].[vwVisitorsAllParks]    Script Date: 9/26/2022 10:05:37 AM ******/
-SET ANSI_NULLS ON
+ALTER TABLE [dbo].[NationalParkList] 
+DROP CONSTRAINT PK_NationalParkList;
 GO
 
-SET QUOTED_IDENTIFIER ON
+ALTER TABLE [dbo].[NationalParkList]
+ADD ID2 int NULL;
 GO
 
-CREATE VIEW [dbo].[vwVisitorsAllParks]
-AS
-SELECT        park_name, park_state, park_code, park_description, park_date_est, park_lat, park_long, park_area_acres, park_area_sq_km, park_visitors, nps_link, pic_license, pic_url
-FROM            dbo.NationalParkList
+UPDATE [dbo].[NationalParkList] SET [dbo].[NationalParkList].ID2 = [dbo].[NationalParkList].ID
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPane1', @value=N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
-Begin DesignProperties = 
-   Begin PaneConfigurations = 
-      Begin PaneConfiguration = 0
-         NumPanes = 4
-         Configuration = "(H (1[41] 4[20] 2[8] 3) )"
-      End
-      Begin PaneConfiguration = 1
-         NumPanes = 3
-         Configuration = "(H (1 [50] 4 [25] 3))"
-      End
-      Begin PaneConfiguration = 2
-         NumPanes = 3
-         Configuration = "(H (1 [50] 2 [25] 3))"
-      End
-      Begin PaneConfiguration = 3
-         NumPanes = 3
-         Configuration = "(H (4 [30] 2 [40] 3))"
-      End
-      Begin PaneConfiguration = 4
-         NumPanes = 2
-         Configuration = "(H (1 [56] 3))"
-      End
-      Begin PaneConfiguration = 5
-         NumPanes = 2
-         Configuration = "(H (2 [66] 3))"
-      End
-      Begin PaneConfiguration = 6
-         NumPanes = 2
-         Configuration = "(H (4 [50] 3))"
-      End
-      Begin PaneConfiguration = 7
-         NumPanes = 1
-         Configuration = "(V (3))"
-      End
-      Begin PaneConfiguration = 8
-         NumPanes = 3
-         Configuration = "(H (1[56] 4[18] 2) )"
-      End
-      Begin PaneConfiguration = 9
-         NumPanes = 2
-         Configuration = "(H (1 [75] 4))"
-      End
-      Begin PaneConfiguration = 10
-         NumPanes = 2
-         Configuration = "(H (1[66] 2) )"
-      End
-      Begin PaneConfiguration = 11
-         NumPanes = 2
-         Configuration = "(H (4 [60] 2))"
-      End
-      Begin PaneConfiguration = 12
-         NumPanes = 1
-         Configuration = "(H (1) )"
-      End
-      Begin PaneConfiguration = 13
-         NumPanes = 1
-         Configuration = "(V (4))"
-      End
-      Begin PaneConfiguration = 14
-         NumPanes = 1
-         Configuration = "(V (2))"
-      End
-      ActivePaneConfig = 0
-   End
-   Begin DiagramPane = 
-      Begin Origin = 
-         Top = 0
-         Left = 0
-      End
-      Begin Tables = 
-         Begin Table = "NationalParkList"
-            Begin Extent = 
-               Top = 6
-               Left = 38
-               Bottom = 307
-               Right = 901
-            End
-            DisplayFlags = 280
-            TopColumn = 0
-         End
-      End
-   End
-   Begin SQLPane = 
-   End
-   Begin DataPane = 
-      Begin ParameterDefaults = ""
-      End
-      Begin ColumnWidths = 9
-         Width = 284
-         Width = 1500
-         Width = 1500
-         Width = 1500
-         Width = 1500
-         Width = 1500
-         Width = 1500
-         Width = 1500
-         Width = 1500
-      End
-   End
-   Begin CriteriaPane = 
-      Begin ColumnWidths = 11
-         Column = 1440
-         Alias = 900
-         Table = 1170
-         Output = 720
-         Append = 1400
-         NewValue = 1170
-         SortType = 1350
-         SortOrder = 1410
-         GroupBy = 1350
-         Filter = 1350
-         Or = 1350
-         Or = 1350
-         Or = 1350
-      End
-   End
-End
-' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vwVisitorsAllParks'
+ALTER TABLE [dbo].[NationalParkList]
+DROP COLUMN ID;
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_DiagramPaneCount', @value=1 , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'VIEW',@level1name=N'vwVisitorsAllParks'
-GO
+EXEC sp_rename 'dbo.NationalParkList.ID2', 'ID', 'COLUMN';
 
-
+PRINT 'SCRIPT EXECUTION COMPLETED';
